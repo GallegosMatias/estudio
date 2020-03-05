c6341289644c8547702e27996a04bd065ae127c2
Matias <gallegosmatiasjoel@gmail.com>
2020-03-05 00:02:14 -0300

    Listas analisis

--- main.js
+++ main.js
@@ -7,4 +7,28 @@
       pager: false,
       controls: false
     });
+
+    $('#funciones').click(function(){
+      $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dGfEuNGbQmymhiQF4oTUIMb");
+    });
+
+    $('#limites').click(function(){
+      $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dG9t51yT4nCwQEtWwCwvBwn");
+    });
+
+    $('#derivadas').click(function(){
+      $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dG2UQ35tPsaVMYkQhc8Vp__");
+    });
+
+    $('#integrales').click(function(){
+      $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dEHnMLZGaNEXgHzJ2-TPLWw");
+    });
+
+    $('#sucesiones').click(function(){
+      $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dHPL4772Xi8NIky1-01D8BH");
+    });
+
+
+
+
   });
