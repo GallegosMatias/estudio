e8d4bc2227ad5c74ad35c96d4b9784d4a5acb7c6
Matias <gallegosmatiasjoel@gmail.com>
2020-03-05 00:43:57 -0300

    Triangulo

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
   $(document).ready(function(){
+    $('#funciones_flecha').hide();
+    $('#derivadas_flecha').hide();
+    $('#integrales_flecha').hide();
+    $('#limites_flecha').hide();
+    $('#sucesiones_flecha').hide();
     $('.slider').bxSlider({
       mode: 'fade',
       captions: true,
@@ -12,12 +17,22 @@
       $('#video_v').fadeOut(300);
       $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dGfEuNGbQmymhiQF4oTUIMb");
+      $('#funciones_flecha').fadeIn(1000);
+      $('#limites_flecha').fadeOut(100);
+      $('#derivadas_flecha').fadeOut(100);
+      $('#integrales_flecha').fadeOut(100);
+      $('#sucesiones_flecha').fadeOut(100);
     });
 
     $('#limites').click(function(){
       $('#video_v').fadeOut(300);
       $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dG9t51yT4nCwQEtWwCwvBwn");
+      $('#funciones_flecha').fadeOut(100);
+      $('#limites_flecha').fadeIn(1000);
+      $('#derivadas_flecha').fadeOut(100);
+      $('#integrales_flecha').fadeOut(100);
+      $('#sucesiones_flecha').fadeOut(100);
 
     });
 
@@ -25,6 +40,11 @@
       $('#video_v').fadeOut(300);
       $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dG2UQ35tPsaVMYkQhc8Vp__");
+      $('#funciones_flecha').fadeOut(100);
+      $('#limites_flecha').fadeOut(100);
+      $('#derivadas_flecha').fadeIn(1000);
+      $('#integrales_flecha').fadeOut(100);
+      $('#sucesiones_flecha').fadeOut(100);
 
     });
 
@@ -32,6 +52,11 @@
       $('#video_v').fadeOut(300);
       $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dEHnMLZGaNEXgHzJ2-TPLWw");
+      $('#funciones_flecha').fadeOut(100);
+      $('#limites_flecha').fadeOut(100);
+      $('#derivadas_flecha').fadeOut(100);
+      $('#integrales_flecha').fadeIn(1000);
+      $('#sucesiones_flecha').fadeOut(100);
 
     });
 
@@ -39,7 +64,11 @@
       $('#video_v').fadeOut(300);
       $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dHPL4772Xi8NIky1-01D8BH");
-
+      $('#funciones_flecha').fadeOut(100);
+      $('#limites_flecha').fadeOut(100);
+      $('#derivadas_flecha').fadeOut(100);
+      $('#integrales_flecha').fadeOut(100);
+      $('#sucesiones_flecha').fadeIn(1000);
     });
 
 
