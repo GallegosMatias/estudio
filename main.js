6eb2c197627bcb50ba962c98362b51f8d6a7e0d6
Matias <gallegosmatiasjoel@gmail.com>
2020-03-05 00:16:56 -0300

    fadeIn fadeOut

--- main.js
+++ main.js
@@ -9,23 +9,37 @@
     });
 
     $('#funciones').click(function(){
+      $('#video_v').fadeOut(300);
+      $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dGfEuNGbQmymhiQF4oTUIMb");
     });
 
     $('#limites').click(function(){
+      $('#video_v').fadeOut(300);
+      $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dG9t51yT4nCwQEtWwCwvBwn");
+
     });
 
     $('#derivadas').click(function(){
+      $('#video_v').fadeOut(300);
+      $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dG2UQ35tPsaVMYkQhc8Vp__");
+
     });
 
     $('#integrales').click(function(){
+      $('#video_v').fadeOut(300);
+      $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dEHnMLZGaNEXgHzJ2-TPLWw");
+
     });
 
     $('#sucesiones').click(function(){
+      $('#video_v').fadeOut(300);
+      $('#video_v').fadeIn(1500);
       $('#video_v').attr("src","https://www.youtube.com/embed/videoseries?list=PLeySRPnY35dHPL4772Xi8NIky1-01D8BH");
+
     });
 
 
