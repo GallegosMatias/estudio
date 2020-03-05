882a07734d8aca48b1fa9b0cc9f818519753c83d
Matias <gallegosmatiasjoel@gmail.com>
2020-03-04 23:34:58 -0300

    Primer commit

---
+++ main.js
@@ -0,0 +1,10 @@
+  $(document).ready(function(){
+    $('.slider').bxSlider({
+      mode: 'fade',
+      captions: true,
+      adaptiveHeight: true,
+      slideWidth: 1920,
+      pager: false,
+      controls: false
+    });
+  });
